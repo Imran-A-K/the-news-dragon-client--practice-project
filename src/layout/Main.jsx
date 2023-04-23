6ce8a2ca612c9115883041b6a451e094e9ba86a0
Imran-A-K <imran.k.professional@gmail.com>
2023-04-23 17:40:40 +0600

    navbar descriptive layout done

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import Header from '../pages/Shared/Header/Header'
 import Footer from '../pages/Shared/Footer/Footer'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import LeftNav from '../pages/Shared/leftNav/leftNav';
+import RightNav from '../pages/Shared/rightNav/RightNav';
+
 
 const Main = () => {
   return (
     <div>
         <Header></Header>
+        
+        <Container>
+      <Row>
+        <Col lg={3}>
+        <LeftNav></LeftNav>
+        </Col>
+        <Col lg={6}>
+        <h2>Main Content Coming.....</h2>
+        </Col>
+        <Col lg={3}>
+        <RightNav></RightNav>
+        </Col>
+      </Row>
+    </Container>
+
         <Footer></Footer>
     </div>
   )
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from "react";
+import logo from "../../../assets/logo.png";
+import moment from "moment";
+import Button from "react-bootstrap/Button";
+import { Container } from "react-bootstrap";
+import Marquee from "react-fast-marquee";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const Header = () => {
   return (
-    <div>
-      <h2>Header</h2>
-    </div>
-  )
-}
+    <Container>
+      <div className="text-center">
+        <img src={logo} />
+        <p className="text-secondary">
+          <small>Journalism without fear or favor</small>
+        </p>
+        <p>{moment().format("dddd, MMMM D, YYYY")}</p>
+      </div>
+      <div className="d-flex">
+      <Button variant="danger">Danger</Button>
+      <Marquee speed={100} className="text-danger">
+        The quick brown fox jumped.       The quick brown fox jumped.          The quick brown fox jumped.
+      </Marquee>
+      </div>
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+      <Container>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mx-auto">
+            <Nav.Link href="#features">Home</Nav.Link>
+            <Nav.Link href="#pricing">About</Nav.Link>
+            <Nav.Link href="#pricing">Career</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Profile</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+            <Button variant="secondary">Login</Button>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </Container>
+  );
+};
 
-export default Header
+export default Header;
