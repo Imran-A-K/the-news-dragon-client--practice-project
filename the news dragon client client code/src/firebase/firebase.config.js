881d6d824a82f40e7f1ad8d6018858df528bf6db
Imran-A-K <imran.k.professional@gmail.com>
2023-04-23 19:40:18 +0600

    q-zone added

---
+++ the news dragon client client code/src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCVTtleWTm0xHiJ5hrQ-3nDAWE9DAsUIMQ",
+  authDomain: "the-news-dragon-93a79.firebaseapp.com",
+  projectId: "the-news-dragon-93a79",
+  storageBucket: "the-news-dragon-93a79.appspot.com",
+  messagingSenderId: "604424186347",
+  appId: "1:604424186347:web:017a688960951f40a311ae"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ the news dragon client client code/src/pages/Shared/leftNav/LeftNav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LeftNav = () => {
+  return (
+    <div>leftNav</div>
+  )
+}
+
+export default LeftNav
