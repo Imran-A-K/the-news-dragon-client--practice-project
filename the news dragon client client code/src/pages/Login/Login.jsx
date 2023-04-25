ae3c9c5dd8df6a809a30abbdf4a9363c95b81150
Imran-A-K <imran.k.professional@gmail.com>
2023-04-25 23:55:44 +0600

    added login and register page

--- the news dragon client client code/src/pages/Login/Login.jsx
+++ the news dragon client client code/src/pages/Login/Login.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import { Container } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <Container className='w-25 mx-auto'>
+        <h3>Please Login</h3>
+        <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" name='email' placeholder="Enter email" required />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" name='password' placeholder="Password" required />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Link>
+      
+      </Link>
+      <Button variant="primary" type="submit">
+        Login
+      </Button>
+      <br />
+      <Form.Text className='text-secondary'>
+        Don't Have an Account?<Link to='/register'>Register</Link>
+      </Form.Text>
+      <Form.Text className='text-success'>
+
+      </Form.Text>
+      <Form.Text className='text-danger'>
+
+      </Form.Text>
+    </Form>
+    </Container>
   )
 }
 
--- the news dragon client client code/src/pages/Register/Register.jsx
+++ the news dragon client client code/src/pages/Register/Register.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
-
+import { Container } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
 const Register = () => {
   return (
-    <div>Register</div>
+    <Container className='w-25 mx-auto'>
+        <h3>Please Register</h3>
+        <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" name='name' placeholder="Your Name" required />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Photo URL</Form.Label>
+        <Form.Control type="text" name='photo' placeholder="Photo URL" required />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" name='email' placeholder="Enter email" required />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" name='password' placeholder="Password" required />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" name='accept' label="Accept Terms And Conditions" />
+        {/* 
+        <Form.Check>
+      <Form.Check.Input type="checkbox" />
+      <Form.Check.Label>
+        <Link to={'/route/' + route}>Navigate to {route}</Link>
+      </Form.Check.Label>
+    </Form.Check>
+        */}
+      </Form.Group>
+      <Link>
+      
+      </Link>
+      <Button variant="primary" type="submit">
+        Register
+      </Button>
+      <br />
+      <Form.Text className='text-secondary'>
+        Already Have an Account?<Link to='/login'>Login</Link>
+      </Form.Text>
+      <Form.Text className='text-success'>
+
+      </Form.Text>
+      <Form.Text className='text-danger'>
+
+      </Form.Text>
+    </Form>
+    </Container>
   )
 }
 
